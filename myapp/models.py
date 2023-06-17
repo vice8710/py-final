c27f91507d2a15d8e0f7131de41743a9690a795f
vice8710 <vicefrancis23@gmail.com>
2023-06-17 23:09:28 +0300

    second commit

--- myapp/models.py
+++ myapp/models.py
@@ -9,6 +9,15 @@ class contact(models.Model):
     subject = models.TextField()
     message = models.TextField(max_length=200)
 
+    #class appointment(models.Model):
+    #    Name = models.CharField(max_length=200)
+    #    email = models.EmailField()
+    #    date = models.DateTimeField()
+    #    message = models.TextField(max_length=200)
+    #    doctor_id = models.CharField()
+    #    phone = models.IntegerField()
+    #    department = models.TextChoices()
+
 
     def __str__(self):
         return self.Name
